2a0e5878bd751bab354ad98725e3bc17be14225f
Zol0tc0m <burmkirill@gmail.com>
2024-05-17 22:41:36 +0300

    Клиент

---
+++ WpfAppTelefon3/MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System.Data;
+using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace WpfAppTelefon3
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+            
+
+           
+        }
+
+        private void Button2_Click(object sender, RoutedEventArgs e)
+        {
+            // Проверяем, что поля TextName и TextIp не пустые
+            if (!string.IsNullOrWhiteSpace(TextName.Text))
+            {
+                // Дополнительная проверка на корректность IP-адреса
+                if (IPAddress.TryParse(TextIp.Text, out var address))
+                {
+                    Window1 window1 = new Window1(TextName.Text); // Передаем имя пользователя
+                    window1.Show();
+                    this.Close();
+                }
+                else
+                {
+                    MessageBox.Show("Некорректный IP-адрес.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Пожалуйста, заполните оба текстовых поля.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (!string.IsNullOrWhiteSpace(TextName.Text))
+            {
+                Process.Start("C:\\Users\\User\\Downloads\\Messenger\\Messenger\\Messenger\\Messenger\\bin\\Debug\\net6.0-windows\\Messenger.exe");
+            }
+            else
+            {
+                // Если одно из полей пустое, показываем MessageBox с ошибкой
+                MessageBox.Show("Пожалуйста, впишите имя.", "Ошибка", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+           
+        }
+    }
+}
---
+++ WpfAppTelefon3/Window1.xaml.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Timers;
+using System.Threading;
+
+namespace WpfAppTelefon3
+{
+    public partial class Window1 : Window
+    {
+        Socket socket;
+        string username;
+        CancellationTokenSource cts = new CancellationTokenSource();
+        List<string> users = new List<string>();
+
+        public Window1(string username)
+        {
+            InitializeComponent();
+            this.username = username;
+            socket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            socket.Connect("26.191.190.98", 2000);
+
+
+
+
+            
+            var joinMessage = $"JOIN {this.username}";
+            var joinBytes = Encoding.UTF8.GetBytes(joinMessage);
+            socket.Send(joinBytes);
+
+            
+            ReceiveMessage(cts.Token);
+        }
+
+
+        private async void SendMessage(string message, CancellationToken token)
+        {
+            if (token.IsCancellationRequested)
+                return;
+
+            if (message == "/disconnect")
+            {
+                var disconnectMessage = $"[{this.username}] покинул чат.";
+                var disconnectBytes = Encoding.UTF8.GetBytes(disconnectMessage);
+                await socket.SendAsync(disconnectBytes, SocketFlags.None, token);
+
+                if (socket.Connected)
+                {
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+                }
+
+                Dispatcher.Invoke(() =>
+                {
+                    MainWindow mainWindow = new MainWindow();
+                    mainWindow.Show();
+                });
+
+                Dispatcher.Invoke(() => this.Close());
+                return;
+            }
+
+            string timestamp = DateTime.Now.ToString("dd.MM.yyyy HH:mm:ss");
+            var formattedMessage = $"[{timestamp} {this.username}]: {message}";
+            var bytes = Encoding.UTF8.GetBytes(formattedMessage);
+
+            await socket.SendAsync(bytes, SocketFlags.None, token);
+        }
+
+        private async void ReceiveMessage(CancellationToken token)
+        {
+            try
+            {
+                while (!token.IsCancellationRequested)
+                {
+                    byte[] bytes = new byte[1024];
+                    await socket.ReceiveAsync(bytes, SocketFlags.None, token);
+                    string message = Encoding.UTF8.GetString(bytes);
+
+                    
+                    if (message.StartsWith("JOIN"))
+                    {
+                        
+                        string newUsername = message.Substring(5).Trim();
+                        users.Add(newUsername);
+                        UpdateUsersList(); 
+                    }
+                    else if (message.StartsWith("LEAVE"))
+                    {
+                        
+                        string leavingUsername = message.Substring(6).Trim();
+                        users.Remove(leavingUsername);
+                        UpdateUsersList(); 
+                    }
+                    else
+                    {
+                        
+                        Dispatcher.Invoke(() => List.Items.Add(message));
+                    }
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                Dispatcher.Invoke(() => List.Items.Add("Получение сообщений было отменено."));
+            }
+            catch (SocketException ex)
+            {
+                Dispatcher.Invoke(() => List.Items.Add($"Ошибка сокета: {ex.Message}"));
+            }
+        }
+
+        
+        private void UpdateUsersList()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                ListUsers.Items.Clear();
+                foreach (string user in users)
+                {
+                    ListUsers.Items.Add(user);
+                }
+            });
+        }
+
+        private void Button1_Click_1(object sender, RoutedEventArgs e)
+        {
+            SendMessage(Text1.Text, cts.Token);
+        }
+
+        private async void Exit_Click(object sender, RoutedEventArgs e)
+        {
+            
+            var leaveMessage = $"LEAVE {this.username}";
+            var leaveBytes = Encoding.UTF8.GetBytes(leaveMessage);
+            await socket.SendAsync(leaveBytes, SocketFlags.None, cts.Token);
+
+            cts.Cancel();
+
+            if (socket.Connected)
+            {
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+                MainWindow window216 = new MainWindow();
+                window216.Show();
+                this.Close();
+            }
+        }
+
+        protected override void OnClosed(EventArgs e)
+        {
+            base.OnClosed(e);
+
+           
+            DisconnectFromChat();
+        }
+
+        private async void DisconnectFromChat()
+        {
+            try
+            {
+                if (socket.Connected)
+                {
+                   
+                    var leaveMessage = $"LEAVE {this.username}";
+                    var leaveBytes = Encoding.UTF8.GetBytes(leaveMessage);
+                    await socket.SendAsync(leaveBytes, SocketFlags.None, cts.Token);
+
+                   
+                    cts.Cancel();
+
+                    
+                    socket.Shutdown(SocketShutdown.Both);
+                    socket.Close();
+
+                    
+                    Dispatcher.Invoke(() =>
+                    {
+                        MainWindow mainWindow = new MainWindow();
+                        mainWindow.Show();
+                        this.Close(); 
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                
+            }
+        }
+    }
+}
